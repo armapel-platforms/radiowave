12788ff2d660c02cf61d3a0c17d4130c979d9be6
Arbie Peligro <arbiemapalopeligro@gmail.com>
2025-09-20 21:01:17 +0800

    Update getStream.js

--- js/getStream.js
+++ js/getStream.js
@@ -2,8 +2,7 @@ window.radioStations = [
     { 
         name: 'DYHP RMN Cebu', 
         logo: 'https://upload.wikimedia.org/wikipedia/en/c/cb/DYHP_612.png',
-        type: 'mp3',
+        type: 'audio/mpeg',
         manifestUri: 'https://de3.amfmph.com/ssl/rmncebu?mp=/stream&1758366700449' 
     }
-    // Add more stations here
 ];
