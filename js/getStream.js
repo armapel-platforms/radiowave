655094415bdd406dd279d23b152aa9834f7a5d35
Arbie Peligro <arbiemapalopeligro@gmail.com>
2025-09-21 07:11:10 +0800

    Delete js/getStream.js

--- js/getStream.js
+++
@@ -1,4 +0,0 @@
-window.radioStations = [
-    { name: 'DYHP RMN Cebu', logo: 'https://upload.wikimedia.org/wikipedia/en/c/cb/DYHP_612.png', type: 'audio/mpeg', manifestUri: 'https://de3.amfmph.com/ssl/rmncebu?mp=/stream&1758366700449' },
-    { name: 'hitsradio.com', logo: 'https://static.mytuner.mobi/media/tvos_radios/552/977-todays-hits.81a51b5b.jpg', type: 'audio/mpeg', manifestUri: 'https://18723.live.streamtheworld.com/977_HITS_SC' }
-];
